fb440779120f86cfcbcbacfe813b1126e9aa1ed1
Andrew Kallai <73764771+andrewkallai@users.noreply.github.com>
2025-06-30 20:42:44 -0400

    Update span.h

--- compiler-rt/lib/inputgen/span.h
+++ compiler-rt/lib/inputgen/span.h
@@ -1,3 +1,14 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===---------------------------------------------------------------------===//
+//
+//Implementation from llvm-project/libcxx/include/span
+
 #include <array>
 #include <cassert>
 #include <cstddef>
